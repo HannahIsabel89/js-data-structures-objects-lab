00473d004a01772ebc2e72b5bb2f5d27037c07cd
Hannah <hannahwoodward@hotmail.com>
2021-10-04 10:14:27 +0800

    tested in terminal and checked answers

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+let driver ={
+    name: "Hannah"
+  }
+  
+  // question one
+  function updateDriverWithKeyAndValue (obj, key, value) {
+    return newDriver = Object.assign({}, obj, {[key]: value});
+  }
+  //console.log(updateDriverWithKeyAndValue(driver, 'address', '11 Broadway'));
+  //console.log(driver['address']);
+  
+  // question 2 
+  function destructivelyUpdateDriverWithKeyAndValue (obj, key, value) {
+    obj.key = value;
+    return obj;
+  };
+  //console.log(destructivelyUpdateDriverWithKeyAndValue(driver, 'address', '12 Broadway'));
+  //console.log(driver);
+  
+  // question 3
+  let newdriver = function deleteFromDriverByKey (obj, key) {
+    const newDriver = Object.assign({}, obj);
+    delete newDriver[key];
+    return newDriver;
+  };
+  
+  console.log(newDriver['name']);
+  console.log(typeof newDriver)
+  console.log(deleteFromDriverByKey(driver, 'name'));
+  console.log(driver['name']);
+  
+  // question 4
+  function destructivelyDeleteFromDriverByKey (obj, key) {
+    delete obj[key];
+    return obj
+  };
